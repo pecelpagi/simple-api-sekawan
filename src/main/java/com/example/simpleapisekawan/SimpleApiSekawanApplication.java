b1c5bfd22b8e7aa08c7ab19bcd2cc00a5e46701d
Galuh Ramadhan <galuh.html@gmail.com>
2024-05-07 11:29:56 +0700

    init commit

---
+++ src/main/java/com/example/simpleapisekawan/SimpleApiSekawanApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleapisekawan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleApiSekawanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleApiSekawanApplication.class, args);
+	}
+
+}
